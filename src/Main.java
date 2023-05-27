81d7630221567881d87a6808364c9541ac23f287
Dodiqell <petrovskih2015@yandex.ru>
2023-05-27 17:05:57 +0500

    1

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main (String[] args) {
+      //Задача 1
+        byte a = -124;
+        short b = 15672;
+        int c = 1333444;
+        long d = -199222555111L;
+        float e = -3.22f;
+        double f = 6.756;
+        System.out.println("Значение переменной byte c типом а равно " + a );
+        System.out.println("Значение переменной short c типом b равно " + b );
+        System.out.println("Значение переменной int c типом c равно " + c );
+        System.out.println("Значение переменной long c типом d равно " + d );
+        System.out.println("Значение переменной float c типом e равно " + e );
+        System.out.println("Значение переменной double c типом f равно " + f );
+        //Задача 2
+
+    }
+}
